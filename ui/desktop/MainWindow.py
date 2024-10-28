1b836437dc679619beff3a509cc827cc02faf427
Fatih YOLCU <kilicsizoglu@gmail.com>
2024-10-28 12:31:33 +0300

    up

--- ui/desktop/MainWindow.py
+++ ui/desktop/MainWindow.py
@@ -16,58 +16,6 @@ from ui.desktop.TopNegativeCoinWindow import TopNegativeCoinWindow
 from ui.desktop.PriceAlertWindow import PriceAlertWindow
 
 class MainWindow(QMainWindow):
-    """
-    MainWindow class for the Crypto Dashboard application.
-
-    This class represents the main window of the Crypto Dashboard application, which provides various functionalities
-    related to cryptocurrency monitoring and analysis. It inherits from QMainWindow and sets up the main user interface
-    with buttons and labels for different features.
-
-    Attributes:
-        key_status_text (QLabel): Label to display the key status.
-        timer (QTimer): Timer to periodically check the lock status.
-        price_alarm_button (QPushButton): Button to open the Price Alarm window.
-        daily_change_button (QPushButton): Button to open the 24h Change window.
-        top_positive_coins_button (QPushButton): Button to open the Top Positive Coins window.
-        top_negative_coins_button (QPushButton): Button to open the Top Negative Coins window.
-        future_position_button (QPushButton): Button to open the Future Position window.
-        key_status_button (QPushButton): Button to open the Key Status window.
-        coin_status_button (QPushButton): Button to open the Average Percentage Change of Coins window.
-        negative_coin_button (QPushButton): Button to open the Negative Coin window.
-        coin_change_analysis_button (QPushButton): Button to open the Coin Change Analysis window.
-        positive_coin_button (QPushButton): Button to open the Positive Coin window.
-        volume_position_button (QPushButton): Button to open the Volume Position window.
-        coin_explorer_button (QPushButton): Button to open the Coin Explorer window.
-        key_status_window (list): List to store instances of the Key Status window.
-        future_position_window (list): List to store instances of the Future Position window.
-        coin_status_window (list): List to store instances of the Coin Status window.
-        negative_coin_window (list): List to store instances of the Negative Coin window.
-        coin_explorer_window (list): List to store instances of the Coin Explorer window.
-        open_coin_change_analysis_window (list): List to store instances of the Coin Change Analysis window.
-        positive_coin_window (list): List to store instances of the Positive Coin window.
-        top_positive_coins_window (list): List to store instances of the Top Positive Coins window.
-        top_negative_coins_window (list): List to store instances of the Top Negative Coins window.
-        volume_position_window (list): List to store instances of the Volume Position window.
-        coin_24h_change_window (list): List to store instances of the 24h Change window.
-        price_alarm_window (list): List to store instances of the Price Alarm window.
-
-    Methods:
-        __init__(): Initializes the main window and sets up the user interface.
-        lock_status(): Checks the lock status of Binance and TAAPI connections and updates the window title.
-        is_key_file(): Checks if the API key file exists and enables/disables buttons accordingly.
-        open_price_alarm_window(): Opens the Price Alarm window.
-        open_top_positive_coins_window(): Opens the Top Positive Coins window.
-        open_top_negative_coins_window(): Opens the Top Negative Coins window.
-        open_future_position_window(): Opens the Future Position window.
-        open_24h_change_window(): Opens the 24h Change window.
-        open_coin_explorer_window(): Opens the Coin Explorer window.
-        open_key_status_window(): Opens the Key Status window.
-        open_coin_status_window(): Opens the Average Percentage Change of Coins window.
-        open_negative_coin_window(): Opens the Negative Coin window.
-        open_coin_change_analysis_window(): Opens the Coin Change Analysis window.
-        open_positive_coin_window(): Opens the Positive Coin window.
-        open_volume_position_window(): Opens the Volume Position window.
-    """
     def __init__(self):
         super().__init__()
 
@@ -141,25 +89,24 @@ class MainWindow(QMainWindow):
         self.is_key_file()
 
         # Instance variables for the windows
-        self.key_status_window = []
-        self.future_position_window = []
-        self.coin_status_window = []
-        self.negative_coin_window = []
-        self.coin_explorer_window = []
-        self.open_coin_change_analysis_window = []
-        self.positive_coin_window = []
-        self.top_positive_coins_window = []
-        self.top_negative_coins_window = []
-        self.volume_position_window = []
-        self.coin_24h_change_window = []
-        self.price_alarm_window = []
+        self.key_status_window = None
+        self.future_position_window = None
+        self.coin_status_window = None
+        self.negative_coin_window = None
+        self.coin_explorer_window = None
+        self.coin_change_analysis_window = None
+        self.positive_coin_window = None
+        self.top_positive_coins_window = None
+        self.top_negative_coins_window = None
+        self.volume_position_window = None
+        self.coin_24h_change_window = None
+        self.price_alarm_window = None
 
     def lock_status(self):
         binanceLock = libs.binanceConnectionLock.BinanceFileLock()
         block = binanceLock.is_locked()
         self.setWindowTitle("Crypto Dashboard - Binance: " + str(block))
 
-    # Functions to open new windows
     def is_key_file(self):
         bc = libs.binanceKeyRW.BinanceAPIKeys()
         if bc.api_key is not None:
@@ -192,73 +139,61 @@ class MainWindow(QMainWindow):
             self.key_status_text.setText("Key Status: No Key File Found")
 
     def open_price_alarm_window(self):
-        self.price_alarm_window.append(PriceAlertWindow())
-        for window in self.price_alarm_window:
-            if not window.isVisible():
-                window.show()
+        if self.price_alarm_window is None or not self.price_alarm_window.isVisible():
+            self.price_alarm_window = PriceAlertWindow()
+            self.price_alarm_window.show()
 
     def open_top_positive_coins_window(self):
-        self.top_positive_coins_window.append(TopPositiveCoinWindow())
-        for window in self.top_positive_coins_window:
-            if not window.isVisible():
-                window.show()
+        if self.top_positive_coins_window is None or not self.top_positive_coins_window.isVisible():
+            self.top_positive_coins_window = TopPositiveCoinWindow()
+            self.top_positive_coins_window.show()
 
     def open_top_negative_coins_window(self):
-        self.top_negative_coins_window.append(TopNegativeCoinWindow())
-        for window in self.top_negative_coins_window:
-            if not window.isVisible():
-                window.show()
+        if self.top_negative_coins_window is None or not self.top_negative_coins_window.isVisible():
+            self.top_negative_coins_window = TopNegativeCoinWindow()
+            self.top_negative_coins_window.show()
 
     def open_future_position_window(self):
-        self.future_position_window.append(FuturePositionWindow())
-        for window in self.future_position_window:
-            if not window.isVisible():
-                window.show()
+        if self.future_position_window is None or not self.future_position_window.isVisible():
+            self.future_position_window = FuturePositionWindow()
+            self.future_position_window.show()
 
     def open_24h_change_window(self):
-        self.coin_24h_change_window.append(Coin24hChangeWindow())
-        for window in self.coin_24h_change_window:
-            if not window.isVisible():
-                window.show()
+        if self.coin_24h_change_window is None or not self.coin_24h_change_window.isVisible():
+            self.coin_24h_change_window = Coin24hChangeWindow()
+            self.coin_24h_change_window.show()
 
     def open_coin_explorer_window(self):
-        self.coin_explorer_window.append(CoinExplorerWindow())
-        for window in self.coin_explorer_window:
-            if not window.isVisible():
-                window.show()
+        if self.coin_explorer_window is None or not self.coin_explorer_window.isVisible():
+            self.coin_explorer_window = CoinExplorerWindow()
+            self.coin_explorer_window.show()
 
     def open_key_status_window(self):
-        self.key_status_window.append(KeyWindow.APIKeyWindow())
-        for window in self.key_status_window:
-            if not window.isVisible():
-                window.show()
+        if self.key_status_window is None or not self.key_status_window.isVisible():
+            self.key_status_window = KeyWindow.APIKeyWindow()
+            self.key_status_window.show()
 
     def open_coin_status_window(self):
-        self.coin_status_window.append(CoinStatusWindow())
-        for window in self.coin_status_window:
-            if not window.isVisible():
-                window.show()
+        if self.coin_status_window is None or not self.coin_status_window.isVisible():
+            self.coin_status_window = CoinStatusWindow()
+            self.coin_status_window.show()
 
     def open_negative_coin_window(self):
-        self.negative_coin_window.append(NegativeCoinWindow())
-        for window in self.negative_coin_window:
-            if not window.isVisible():
-                window.show()
+        if self.negative_coin_window is None or not self.negative_coin_window.isVisible():
+            self.negative_coin_window = NegativeCoinWindow()
+            self.negative_coin_window.show()
 
     def open_coin_change_analysis_window(self):
-        self.open_coin_change_analysis_window.append(CoinChangeAnalysisWindow())
-        for window in self.open_coin_change_analysis_window:
-            if not window.isVisible():
-                window.show()
+        if self.coin_change_analysis_window is None or not self.coin_change_analysis_window.isVisible():
+            self.coin_change_analysis_window = CoinChangeAnalysisWindow()
+            self.coin_change_analysis_window.show()
 
     def open_positive_coin_window(self):
-        self.positive_coin_window.append(PositiveCoinWindow())
-        for window in self.positive_coin_window:
-            if not window.isVisible():
-                window.show()
+        if self.positive_coin_window is None or not self.positive_coin_window.isVisible():
+            self.positive_coin_window = PositiveCoinWindow()
+            self.positive_coin_window.show()
 
     def open_volume_position_window(self):
-        self.volume_position_window.append(VolumePositionWindow())
-        for window in self.volume_position_window:
-            if not window.isVisible():
-                window.show()
+        if self.volume_position_window is None or not self.volume_position_window.isVisible():
+            self.volume_position_window = VolumePositionWindow()
+            self.volume_position_window.show()
